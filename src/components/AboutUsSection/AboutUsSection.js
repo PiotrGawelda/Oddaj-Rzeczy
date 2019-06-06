bed6fb925304d281494f2627914164774d7f22ac
PiotrGawelda <piotr.gawelda@gmail.com>
2019-06-06 17:48:17 +0200

    AboutUsSection update

--- src/components/AboutUsSection/AboutUsSection.js
+++ src/components/AboutUsSection/AboutUsSection.js
@@ -5,10 +5,11 @@ class AboutUsSection extends Component {
     render() {
         return(
             <section className="about-us-section">
-                <div className="container about-us-section">
+                <div className="container block-center">
                     <div className="about-us-info">
                         <h2>O nas</h2>
                         <img src="../.././images/decoration.png" alt="Black frame"/>
+                        <p>Lorem ipsum dolor sit amet enim. Etiam ullamcorper. Suspendisse a pellentesque dui, non felis.  Curabitur et ligula. Ut molestie a, ultricies porta urna.</p>
                     </div>
                     <div className="about-us-image">
                         <img src="../../images/about.jpg" alt="People"/>
