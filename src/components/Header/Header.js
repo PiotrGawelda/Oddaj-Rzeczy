e02339ed6e7fbf9f64a9f5a2de702fa0405e46fc
PiotrGawelda <piotr.gawelda@gmail.com>
2019-06-07 17:03:18 +0200

    Font and help-section-style update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,10 @@ import Links from "./Links";
 class Header extends Component {
     render() {
         return(
-            <Links/>
+            <>
+                <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet"/>
+                <Links/>
+            </>
         )
     }
 }
