1d3a7c76a881b6e9184ec1d3a2aa4c10a7d9aed6
PiotrGawelda <piotr.gawelda@gmail.com>
2019-06-25 13:34:04 +0200

    StepsItemSection update

--- src/components/StepsItemSection/StepsItemSection.js
+++ src/components/StepsItemSection/StepsItemSection.js
@@ -3,7 +3,32 @@ import React, {Component} from "react";
 class StepsItemSection extends Component{
     render() {
         return(
-            <h1>Hello</h1>
+            <section className="steps-item-section">
+                <div className="steps-item-block">
+                    <p>Krok 1/4</p>
+                    <h2>Zaznacz co chcesz oddać:</h2>
+                    <form className="steps-form">
+                        <label>
+                            <input type="checkbox" className="checkbox"/> ubrania, które nadają się do ponownego użycia
+                        </label>
+                        <label>
+                            <input type="checkbox"/> ubrania, do wyrzucenia
+                        </label>
+                        <label>
+                            <input type="checkbox"/> zabawki
+                        </label>
+                        <label>
+                            <input type="checkbox"/> książki
+                        </label>
+                        <label>
+                            <input type="checkbox"/> inne
+                        </label>
+                        <label>
+                            <input type="submit" value="Dalej" id="steps-btn"/>
+                        </label>
+                    </form>
+                </div>
+            </section>
         )
     }
 }
