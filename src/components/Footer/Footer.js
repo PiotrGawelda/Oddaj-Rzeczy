40861a1bf40ebb0c5df54f16283acd862b43f213
PiotrGawelda <piotr.gawelda@gmail.com>
2019-06-26 22:35:46 +0200

    Footer.js update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,9 +4,30 @@ import Decoration from "../Decoration";
 import FooterImg from './../../images/footer_bg_img.jpg';
 
 class Footer extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            name: "",
+            nameError: "Proszę podać imię",
+            password: "",
+            passwordError: "",
+            text: "",
+            textError: ""
+        }
+    }
+
+    handleChange = (e) => {
+        e.preventDefault();
+
+    };
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+    };
+
     render() {
         return(
-            <div className="footer" style={{backgroundImage: `linear-gradient(to right, rgba(155,155,155,0) 46%,rgba(155,155,155,0.22) 58%,rgba(165,165,165,1) 100%), url(${FooterImg})`}}>
+            <div className="footer" style={{backgroundImage: `linear-gradient(to right, rgba(255,255,255,0) 61%,rgba(255,255,255,0) 67%,rgba(255,255,255,1) 100%), url(${FooterImg})`}}>
                 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
                       integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay"
                       crossOrigin="anonymous"/>
@@ -18,11 +39,11 @@ class Footer extends Component {
                         </div>
                         <div className="footer-form">
                             <p>FORMULARZ KONTAKTOWY</p>
-                            <input type="text" name="footer-form" placeholder="Imie"/>
-                            <input type="text" name="footer-form" placeholder="Email"/>
-                            <input type="text" name="footer-form" placeholder="Wiadomość"/>
+                            <input type="text" name="footer-form" placeholder="Imie" onChange={this.handleChange}/>
+                            <input type="text" name="footer-form" placeholder="Email" onChange={this.handleChange}/>
+                            <input type="text" name="footer-form" placeholder="Wiadomość" onChange={this.handleChange}/>
                             <div className="form-button">
-                                <button name="footer-form">Wyślij</button>
+                                <button name="footer-form" onClick={this.handleSubmit}>Wyślij</button>
                             </div>
                         </div>
                     </div>
