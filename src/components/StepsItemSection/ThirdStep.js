91c6c74386330d01134ed9bf5fd350a28e64b1f8
PiotrGawelda <piotr.gawelda@gmail.com>
2019-07-05 15:44:09 +0200

    ThirdStep update

--- src/components/StepsItemSection/ThirdStep.js
+++ src/components/StepsItemSection/ThirdStep.js
@@ -28,13 +28,13 @@ class ThirdStep extends Component{
                 <p>Krok 3/4</p>
                 <h2>Lokalizacja</h2>
                 <form className="steps-form">
-                <select>
+                <select className="third-select">
                     <option>
                         - wybierz -
                     </option>
                 </select>
                 <div className="btn-choice-block">
-                    <h4> Komu chcesz pomóc?</h4>
+                    <h4 className="bold-font"> Komu chcesz pomóc?</h4>
                     <button className="btn-choice">dzieciom</button>
                     <button className="btn-choice">samotnym matkom</button>
                     <button className="btn-choice">bezdomnym</button>
@@ -42,8 +42,10 @@ class ThirdStep extends Component{
                     <button className="btn-choice">osobom starszym</button>
                 </div>
                 <div className="btn-optional">
-                    <h4>Wpisz nazwę konkretnej organizacji (opcjonalne)</h4>
-                    <input type="text" name="text-organisation"/>
+                    <div className="input-position">
+                        <h4 className="bold-font">Wpisz nazwę konkretnej organizacji (opcjonalne)</h4>
+                        <input type="text" name="text-organisation"/>
+                    </div>
                     <input type="submit" value="Wstecz" className="btn-position steps-btn" />
                     <input type="submit" value="Dalej"  className="btn-position steps-btn"  />
                 </div>
